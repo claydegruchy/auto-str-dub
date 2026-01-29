56833316f1028c2ceb6a18b328cac3560e59c77b
Clay DeGruchy <clay.degruchy+github@gmail.com>
2026-01-29 22:47:02 +0100

    add dryrun mode

--- main.py
+++ main.py
@@ -76,7 +76,8 @@ def dict_to_srt(parsed_srt):
 def main():
 	parser = argparse.ArgumentParser(description="Run XTTS from text/SRT file")
 	parser.add_argument("srt", help="Path to input text or SRT file")
-	parser.add_argument("video", default="none.wav", help="Video for this SRT file")
+	parser.add_argument("video", help="Video for this SRT file")
+	parser.add_argument("--dryrun", action="store_true", help="Dryrun mode")
 
 	args = parser.parse_args()
 
@@ -123,15 +124,13 @@ def main():
 	done_files = [x.name for x in list(work_dir.glob("*.wav"))]
 	print("starting", text_path.name, len(done_files), "files already completed")
 	# exit()
-	# tts = init_tts()
+	tts=None
+	
 	str_json=srt_to_dict(text)
 	i=0
 	print("Parsed file", len(str_json), "blocks to complete")
 	for block in str_json:
 		print("starting block", i,"out of" ,len(str_json))
-		if i>3:
-			print("limit reached")
-			break
 		i+=1
 		filename ="_"+ block["index"] + ".wav"
 		print(block)
@@ -146,12 +145,15 @@ def main():
 
 		print("making audio",block["text"],path)
 		# # Run TTS to file
-		# tts.tts_to_file(
-		# 	text=block["text"],
-		# 	file_path=path,
-		# 	speaker_wav=base_params["speaker_wav"],
-		# 	language=base_params["language_idx"]
-		# )
+		if not tts and not args.dryrun:
+			tts = init_tts()
+		if tts:
+			tts.tts_to_file(
+				text=block["text"],
+				file_path=path,
+				speaker_wav=base_params["speaker_wav"],
+				language=base_params["language_idx"]
+			)
 	print("tts done")
 
 	print("making combined audio file")
@@ -187,6 +189,9 @@ def main():
 	print("updating video with new audio")
 
 	video_output_path = "dubbed "+video_path.name
+	
+	print(video_path.parent)
+	exit()
 	video = VideoFileClip(video_path)
 
 	# Load audio
